c0358f3c8e985a8e251d35801bacd18de5d5a57b
Stefan Krasser <stefan.krasser@edu.fh-joanneum.at>
2017-12-09 21:40:44 +0100

    added test to TimesheetSpec

--- calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
+++ calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
@@ -1,11 +1,15 @@
 package at.fhj.swengb.apps.calculator
 
-import scala.util.Try
+import java.util.NoSuchElementException
+
+import scala.util.{Success, Try}
 
 /**
   * Companion object for our reverse polish notation calculator.
   */
 object RpnCalculator {
+
+  val calc: RpnCalculator = RpnCalculator()
 
   /**
     * Returns empty RpnCalculator if string is empty, otherwise pushes all operations
@@ -14,56 +18,73 @@ object RpnCalculator {
     * @param s a string representing a calculation, for example '1 2 +'
     * @return
     */
-  def apply(s: String): Try[RpnCalculator] = ???
 
-}
+  def apply(s: String): Try[RpnCalculator] = {
 
-/**
-  * Reverse Polish Notation Calculator.
-  *
-  * @param stack a datastructure holding all operations
-  */
-case class RpnCalculator(stack: List[Op] = Nil) {
-
-  /**
-    * By pushing Op on the stack, the Op is potentially executed. If it is a Val, it the op instance is just put on the
-    * stack, if not then the stack is examined and the correct operation is performed.
-    *
-    * @param op
-    * @return
-    */
-  def push(op: Op): Try[RpnCalculator] = {
+    if (s == "")
+      Try(calc)
+    else {
+      //val help = s.split(" ")
+      //calc.push(test)
+      Try(calc)
+    }
 
   }
-
-  /**
-    * Pushes val's on the stack.
-    *
-    * If op is not a val, pop two numbers from the stack and apply the operation.
-    *
-    * @param op
-    * @return
-    */
-  def push(op: Seq[Op]): Try[RpnCalculator] = ???
-
-  /**
-    * Returns an tuple of Op and a RevPolCal instance with the remainder of the stack.
-    *
-    * @return
-    */
-  def pop(): (Op, RpnCalculator) = ???
-
-  /**
-    * If stack is nonempty, returns the top of the stack. If it is empty, this function throws a NoSuchElementException.
-    *
-    * @return
-    */
-  def peek(): Op = ???
-
-  /**
-    * returns the size of the stack.
-    *
-    * @return
-    */
-  def size: Int = ???
 }
+  /**
+    * Reverse Polish Notation Calculator.
+    *
+    * @param stack a datastructure holding all operations
+    */
+  case class RpnCalculator(stack: List[Op] = Nil) {
+
+    /**
+      * By pushing Op on the stack, the Op is potentially executed. If it is a Val, it the op instance is just put on the
+      * stack, if not then the stack is examined and the correct operation is performed.
+      *
+      * @param op
+      * @return
+      */
+    def push(op: Op): Try[RpnCalculator] = {
+      //push(stack)
+      //stack +: op
+      Try(this)
+    }
+
+    /**
+      * Pushes val's on the stack.
+      *
+      * If op is not a val, pop two numbers from the stack and apply the operation.
+      *
+      * @param op
+      * @return
+      */
+    def push(op: Seq[Op]): Try[RpnCalculator] = ???
+
+    /**
+      * Returns an tuple of Op and a RevPolCal instance with the remainder of the stack.
+      *
+      * @return
+      */
+    def pop(): (Op, RpnCalculator) = ???
+
+    /**
+      * If stack is nonempty, returns the top of the stack. If it is empty, this function throws a NoSuchElementException.
+      *
+      * @return
+      */
+    def peek(): Op = {
+      if (stack.nonEmpty)
+        stack.head
+      else
+        throw new NoSuchElementException("empty")
+    }
+
+    /**
+      * returns the size of the stack.
+      *
+      * @return
+      */
+    def size: Int = stack.size
+  }
+
--- calculator/src/test/scala/at/fhj/swengb/apps/calculator/TimesheetSpec.scala
+++ calculator/src/test/scala/at/fhj/swengb/apps/calculator/TimesheetSpec.scala
@@ -1,31 +1,54 @@
 package at.fhj.swengb.apps.calculator
 
+import java.nio.file.{Files, Path, Paths}
+import java.util
+import scala.collection.JavaConverters._
+
 import org.scalatest.WordSpecLike
+
+//import scala.reflect.io.Path
+
 
 class TimesheetSpec extends WordSpecLike {
 
   val expected =
-    """== Time expenditure: Calculator assignment
-      |
-      |[cols="1,1,4", options="header"]
-      |.Time expenditure
-      ||===
-      || Date
-      || Hours
-      || Description
-      |
-      || 29.11.17
-      || 1
-      || Review of this and that
-      |
-      || 30.11.17
-      || 5
-      || implemented css
-      |
-      || 11.07.17
-      || 2
-      || fixed bugs
-      |
-      ||===
-      |""".stripMargin
+    """|== Time expenditure: Calculator assignment
+       |
+       |[cols="1,1,4", options="header"]
+       |.Time expenditure
+       ||===
+       || Date
+       || Hours
+       || Description
+       |
+       || 2.12.17
+       || 2
+       || Created the GUI
+       |
+       || 5.12.17
+       || 2
+       || Finished the GUI and implemented some button functionality
+       |
+       |
+       ||===
+       |""".stripMargin
+
+
+
+  "content is equal" in {
+    val path: Path = Paths.get("D:\\Dokumente\\FH\\SWENGB\\workspace\\fhj.swengb2017.assignments\\calculator\\timesheet-calculator.adoc")
+    val content: util.List[String] = Files.readAllLines(path)
+    var lines: String = ""
+    val result = content.asScala.mkString("\n")
+
+    for (l <- content.asScala) {
+      //println(l)
+      lines += l
+      lines += "\n"
+    }
+    println(lines)
+    assert(expected.equals(result))
+  }
+
+
 }
