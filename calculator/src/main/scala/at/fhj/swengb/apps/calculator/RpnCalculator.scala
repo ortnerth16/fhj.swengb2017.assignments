5e95fbdef97d0d593d59604e24620685be57e634
ortnerth16 <thomas.ortner2@edu.fh-joanneum.at>
2017-12-11 20:14:36 +0100

    Final Calculator version

--- calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
+++ calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
@@ -1,6 +1,8 @@
 package at.fhj.swengb.apps.calculator
 
 import scala.util.Try
+import scala.util.matching.Regex.Match
+
 
 /**
   * Companion object for our reverse polish notation calculator.
@@ -14,7 +16,10 @@ object RpnCalculator {
     * @param s a string representing a calculation, for example '1 2 +'
     * @return
     */
-  def apply(s: String): Try[RpnCalculator] = ???
+  def apply(s: String): Try[RpnCalculator] = {
+        if(s.isEmpty) Try(RpnCalculator(Nil))
+        else RpnCalculator().push(s.split(' ').map((str) => Op(str)).toSeq)
+  }
 
 }
 
@@ -32,7 +37,47 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     * @param op
     * @return
     */
-  def push(op: Op): Try[RpnCalculator] = ???
+  def push(op: Op): Try[RpnCalculator] = {
+    op match {
+      case Val(x) => Try(RpnCalculator(stack :+Val(x)))
+      case Add => {
+        pop()._1 match {
+          case Val(x) => pop()._2.pop()._1 match {
+            case Val(y) => Try(RpnCalculator(stack.init.init :+Add.eval(Val(y), Val(x))))
+            case _ => Try(this)
+          }
+          case _ => Try(this)
+        }
+      }
+      case Sub => {
+        pop()._1 match {
+          case Val(x) => pop()._2.pop()._1 match {
+            case Val(y) => Try(RpnCalculator(stack.init.init :+Sub.eval(Val(y), Val(x))))
+            case _ => Try(this)
+          }
+          case _ => Try(this)
+        }
+      }
+      case Mul => {
+        pop()._1 match {
+          case Val(x) => pop()._2.pop()._1 match {
+            case Val(y) => Try(RpnCalculator(stack.init.init :+Mul.eval(Val(y), Val(x))))
+            case _ => Try(this)
+          }
+          case _ => Try(this)
+        }
+      }
+      case Div => {
+        pop()._1 match {
+          case Val(x) => pop()._2.pop()._1 match {
+            case Val(y) => Try(RpnCalculator(stack.init.init :+Div.eval(Val(y), Val(x))))
+            case _ => Try(this)
+          }
+          case _ => Try(this)
+        }
+      }
+    }
+  }
 
   /**
     * Pushes val's on the stack.
@@ -42,26 +87,33 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     * @param op
     * @return
     */
-  def push(op: Seq[Op]): Try[RpnCalculator] = ???
+  def push(op: Seq[Op]): Try[RpnCalculator] = {
+    Try(op.foldLeft(RpnCalculator(stack))((ack,x) => ack.push(x).get))
+  }
+
 
   /**
     * Returns an tuple of Op and a RevPolCal instance with the remainder of the stack.
     *
     * @return
     */
-  def pop(): (Op, RpnCalculator) = ???
+  def pop(): (Op, RpnCalculator) = (peek(),RpnCalculator(stack.init))
 
   /**
     * If stack is nonempty, returns the top of the stack. If it is empty, this function throws a NoSuchElementException.
     *
     * @return
     */
-  def peek(): Op = ???
+  def peek(): Op ={
+    if (stack.size != 0) stack(stack.size-1) else {
+      throw new NoSuchElementException
+    }
+  }
 
   /**
     * returns the size of the stack.
     *
     * @return
     */
-  def size: Int = ???
+  def size: Int = stack.size
 }
