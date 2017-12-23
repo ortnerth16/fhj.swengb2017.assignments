25fe7b28237ca6eb3d2cb98a2935184cc75bd204
Stefan Krasser <stefan.krasser@edu.fh-joanneum.at>
2017-12-23 12:14:36 +0100

    Implemented convert functions

--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
@@ -1,11 +1,35 @@
 package at.fhj.swengb.apps.battleship
 
-import at.fhj.swengb.apps.battleship.model.BattleShipGame
+import at.fhj.swengb.apps.battleship.BattleShipProtobuf.{BattlePos, Direction}
+import at.fhj.swengb.apps.battleship.model.{BattleShipGame, Horizontal, Vertical, Vessel, Fleet}
 
 object BattleShipProtocol {
 
-  def convert(g : BattleShipGame) : BattleShipProtobuf.BattleShipGame = BattleShipProtobuf.BattleShipGame.newBuilder().build()
+  def convert(g : BattleShipGame) : BattleShipProtobuf.BattleShipGame = ???
 
   def convert(g : BattleShipProtobuf.BattleShipGame) : BattleShipGame = ???
+
+  def convert(g: Vessel) : BattleShipProtobuf.Vessel = {
+    val direction = {
+      g.direction match {
+        case Horizontal => Direction.Horizontal
+        case Vertical => Direction.Vertical
+      }
+    }
+
+    BattleShipProtobuf.Vessel.newBuilder()
+      .setName(g.name.value)
+      .setStartPos(BattlePos.newBuilder().setX(g.startPos.x).setY(g.startPos.y))
+      .setDirection(direction)
+      .setSize(g.size).build()
+  }
+
+  def convert(g: Fleet) : BattleShipProtobuf.Fleet = ???
+
+  /*def convert(g: BattlePos) : BattleShipProtobuf.BattlePos = {
+    BattleShipProtobuf.BattlePos.newBuilder().setX(g.getX).setY(g.getY).build()
+  }
+
+  def convert(g: BattleShipProtobuf.BattlePos) : BattlePos = ???*/
 
 }
