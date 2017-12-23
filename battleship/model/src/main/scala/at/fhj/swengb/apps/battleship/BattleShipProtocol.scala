54ba341025c868a1307a60285d26bd977570f06c
Stefan Krasser <stefan.krasser@edu.fh-joanneum.at>
2017-12-23 20:31:40 +0100

    Convert back Funktionen implementiert 2 Tests hinzugefügt

--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
@@ -1,23 +1,23 @@
 package at.fhj.swengb.apps.battleship
 
 import scala.collection.JavaConverters._
-import at.fhj.swengb.apps.battleship.BattleShipProtobuf.{BattlePos, Direction}
+import at.fhj.swengb.apps.battleship.BattleShipProtobuf.Direction
 import at.fhj.swengb.apps.battleship.model._
 
 object BattleShipProtocol {
 
   // Convert to BattleShipProtobuf
 
-  def convert(g : BattleShipGame) : BattleShipProtobuf.BattleShipGame = {
+  def convert(g: BattleShipGame): BattleShipProtobuf.BattleShipGame = {
     BattleShipProtobuf.BattleShipGame.newBuilder()
-        .setBattlefield(convert(g.battleField))
-        .setCellWidth(g.battleField.width)
-        .setCellHeight(g.battleField.height)
-        .setLog(g.log.toString()).build()
+      .setBattlefield(convert(g.battleField))
+      .setCellWidth(g.battleField.width)
+      .setCellHeight(g.battleField.height)
+      .setLog(g.log.toString()).build()
 
   }
 
-  def convert(g: Vessel) : BattleShipProtobuf.Vessel = {
+  def convert(g: Vessel): BattleShipProtobuf.Vessel = {
     val direction = {
       g.direction match {
         case Horizontal => Direction.Horizontal
@@ -27,27 +27,45 @@ object BattleShipProtocol {
 
     BattleShipProtobuf.Vessel.newBuilder()
       .setName(g.name.value)
-      .setStartPos(BattlePos.newBuilder().setX(g.startPos.x).setY(g.startPos.y))
+      .setStartPos(convert(g.startPos))
       .setDirection(direction)
       .setSize(g.size).build()
   }
 
-  def convert(g: Fleet) : BattleShipProtobuf.Fleet = {
+  def convert(g: Fleet): BattleShipProtobuf.Fleet = {
     BattleShipProtobuf.Fleet.newBuilder().addAllVessel(g.vessels.map(convert).asJava).build()
   }
 
-  def convert(g: BattleField) : BattleShipProtobuf.BattleField = {
+  def convert(g: BattleField): BattleShipProtobuf.BattleField = {
     BattleShipProtobuf.BattleField.newBuilder()
       .setFleet(convert(g.fleet))
       .setWith(g.width)
       .setHeight(g.height).build()
   }
 
-  def convert(g: BattlePos) : BattleShipProtobuf.BattlePos = BattleShipProtobuf.BattlePos.newBuilder().setX(g.getX).setY(g.getY).build()
+  def convert(g: BattlePos): BattleShipProtobuf.BattlePos = BattleShipProtobuf.BattlePos.newBuilder().setX(g.x).setY(g.y).build()
 
   // Convert back
+  def convert(g: BattleShipProtobuf.Vessel): Vessel = {
+    val direction = {
+      g.getDirection match {
+        case Direction.Horizontal => Horizontal
+        case Direction.Vertical => Vertical
+      }
+    }
+      Vessel(NonEmptyString(g.getName), BattlePos(g.getStartPos.getX, g.getStartPos.getY), direction, g.getSize)
 
-  def convert(g : BattleShipProtobuf.BattleShipGame) : BattleShipGame = ???
+  }
 
+  def convert(g: BattleShipProtobuf.Fleet): Fleet = {
+    val vessels: Set[Vessel] = g.getVesselList.asScala.map(x => convert(x)).toSet
+    Fleet(vessels)
+  }
 
+  def convert(g: BattleShipProtobuf.BattleField) : BattleField = BattleField(g.getWith, g.getHeight, convert(g.getFleet))
+
+  def convert(g: BattleShipProtobuf.BattleShipGame): BattleShipGame = BattleShipGame(convert(g.getBattlefield), (x: Int) => x.toDouble, (x: Int) => x.toDouble, x => ())
+
+  def convert(g: BattleShipProtobuf.BattlePos): BattlePos = BattlePos(g.getX, g.getY)
 }
+
--- battleship/model/src/test/scala/at/fhj/swengb/apps/battleship/BattleShipGameProtocolSpec.scala
+++ battleship/model/src/test/scala/at/fhj/swengb/apps/battleship/BattleShipGameProtocolSpec.scala
@@ -1,23 +1,55 @@
 package at.fhj.swengb.apps.battleship
 
-import at.fhj.swengb.apps.battleship.model.BattleShipGame
+import at.fhj.swengb.apps.battleship.BattleShipProtobuf.Direction
+import at.fhj.swengb.apps.battleship.model._
+import at.fhj.swengb.apps.battleship.BattleShipProtocol
 import org.scalacheck.{Gen, Prop}
 import org.scalatest.WordSpecLike
 import org.scalatest.prop.Checkers
 
 
-class BattleShipProtocolSpec extends WordSpecLike {
+class BattleShipGameProtocolSpec extends WordSpecLike {
 
-  import at.fhj.swengb.apps.battleship.model.BattleshipGameGen._
 
   "BattleShipProtocol" should {
-    "be deserializable" in {
+    /*"be deserializable" in {
       Checkers.check(Prop.forAll(battleShipGameGen) {
         expected: BattleShipGame => {
           val actual = BattleShipProtocol.convert(BattleShipProtocol.convert(expected))
           actual == expected
         }
       })
+    }*/
+    ".convert(pos : BattlePos)" in {
+      val expectedPos = BattlePos(4711, 815)
+      val actualPos: BattleShipProtobuf.BattlePos = BattleShipProtocol.convert(expectedPos)
+
+      assert(actualPos.getX == expectedPos.x)
+      assert(actualPos.getY == expectedPos.y)
     }
+
+    ".convert(vessel : Vessel)" in {
+      val expectedVessel = Vessel(NonEmptyString("Peter"), BattlePos(0,0), Horizontal, 2)
+      val actualVessel : BattleShipProtobuf.Vessel = BattleShipProtocol.convert(expectedVessel)
+
+      val direction = {
+        actualVessel.getDirection match {
+          case Direction.Horizontal => Horizontal
+          case Direction.Vertical => Vertical
+        }
+      }
+
+      assert(actualVessel.getStartPos.getX == expectedVessel.startPos.x)
+      assert(actualVessel.getStartPos.getY == expectedVessel.startPos.y)
+
+      assert(direction == expectedVessel.direction)
+
+      assert(actualVessel.getName == expectedVessel.name.value)
+
+      assert(actualVessel.getSize == expectedVessel.size)
+    }
+   /* ".convert(fleet : Fleet)" in {
+      val expectedFleet = Fleet(vessels = )
+    }*/
   }
 }
